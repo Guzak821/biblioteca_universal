64ac0a6a84b793fd08721cd6fc882f0f9517d8fe
Principal <Guzak821>
2025-11-15 15:24:36 -0600

    login terminado en front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 // src/App.tsx
 
 import React from 'react';
-// La ruta es relativa a src/, asumiendo esta estructura:
 // src/components/Header/Header.tsx
 import Header from './header/Header'; 
 // src/login/Login.tsx
@@ -12,12 +11,6 @@ const App: React.FC = () => {
   return (
     // Usa un contenedor general para envolver toda la aplicación
     <div className="app-container">
-      
-      {/* 1. Renderiza el Header */}
-      <Header 
-        // En la pantalla de login, el header puede ir vacío o mostrar un enlace de registro
-        // rightContent={<div><a href="/register">Registrarse</a></div>}
-      /> 
       
       {/* 2. Renderiza la página de Login */}
       <Login />
